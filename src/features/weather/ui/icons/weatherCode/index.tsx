b976317bc6f8d9ea7974bed6ddcd935167ed9e0e
Xulik <visahomet@gmail.com>
2025-04-04 10:42:48 +0300

    add icon WeatherCode

---
+++ src/features/weather/ui/icons/weatherCode/index.tsx
@@ -0,0 +1,29 @@
+import { ClearDayIcon } from './clear-day'
+import { ClearNightIcon } from './clear-night'
+import { CloudIcon } from './cloud'
+import { DrizzleIcon } from './drizzle'
+import { FogDayIcon } from './fog-day'
+import { FogNightIcon } from './fog-night'
+import { FreezingRainIcon } from './freezing-rain'
+import { PartlyCloudDayIcon } from './partly-cloud-day'
+import { PartlyCloudNightIcon } from './partly-cloud-night'
+import { RainIcon } from './rain'
+import { SnowIcon } from './snow'
+import { SnowShowersIcon } from './snow-showers'
+import { ThunderstormIcon } from './thunderstorm'
+
+export {
+  ClearDayIcon,
+  ClearNightIcon,
+  CloudIcon,
+  DrizzleIcon,
+  FogDayIcon,
+  FogNightIcon,
+  FreezingRainIcon,
+  PartlyCloudDayIcon,
+  PartlyCloudNightIcon,
+  RainIcon,
+  SnowIcon,
+  SnowShowersIcon,
+  ThunderstormIcon
+}
