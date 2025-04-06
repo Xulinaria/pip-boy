bf80f2f7daa41017c69016ac178149a531721707
Xulik <visahomet@gmail.com>
2025-04-06 21:57:19 +0300

    add: icon for weather-code

---
+++ src/entities/weather/assets/icons/index.tsx
@@ -0,0 +1,29 @@
+import { ClearDayIcon } from './weatherCode/clear-day'
+import { ClearNightIcon } from './weatherCode/clear-night'
+import { CloudIcon } from './weatherCode/cloud'
+import { DrizzleIcon } from './weatherCode/drizzle'
+import { FogDayIcon } from './weatherCode/fog-day'
+import { FogNightIcon } from './weatherCode/fog-night'
+import { FreezingRainIcon } from './weatherCode/freezing-rain'
+import { PartlyCloudDayIcon } from './weatherCode/partly-cloud-day'
+import { PartlyCloudNightIcon } from './weatherCode/partly-cloud-night'
+import { RainIcon } from './weatherCode/rain'
+import { SnowIcon } from './weatherCode/snow'
+import { SnowShowersIcon } from './weatherCode/snow-showers'
+import { ThunderstormIcon } from './weatherCode/thunderstorm'
+
+export {
+  ClearDayIcon,
+  ClearNightIcon,
+  CloudIcon,
+  DrizzleIcon,
+  FogDayIcon,
+  FogNightIcon,
+  FreezingRainIcon,
+  PartlyCloudDayIcon,
+  PartlyCloudNightIcon,
+  RainIcon,
+  SnowIcon,
+  SnowShowersIcon,
+  ThunderstormIcon
+}
