a6d06538c407b449d991e72df236f6e875718455
Xulik <visahomet@gmail.com>
2025-04-08 19:08:04 +0300

    update index-icon: add import weather-stats-icon

--- src/entities/weather/assets/icons/index.tsx
+++ src/entities/weather/assets/icons/index.tsx
@@ -1,16 +1,22 @@
-import { ClearDayIcon } from './weatherCode/clear-day'
-import { ClearNightIcon } from './weatherCode/clear-night'
-import { CloudIcon } from './weatherCode/cloud'
-import { DrizzleIcon } from './weatherCode/drizzle'
-import { FogDayIcon } from './weatherCode/fog-day'
-import { FogNightIcon } from './weatherCode/fog-night'
-import { FreezingRainIcon } from './weatherCode/freezing-rain'
-import { PartlyCloudDayIcon } from './weatherCode/partly-cloud-day'
-import { PartlyCloudNightIcon } from './weatherCode/partly-cloud-night'
-import { RainIcon } from './weatherCode/rain'
-import { SnowIcon } from './weatherCode/snow'
-import { SnowShowersIcon } from './weatherCode/snow-showers'
-import { ThunderstormIcon } from './weatherCode/thunderstorm'
+//weather-icon
+import { ClearDayIcon } from './weather-code/clear-day'
+import { ClearNightIcon } from './weather-code/clear-night'
+import { CloudIcon } from './weather-code/cloud'
+import { DrizzleIcon } from './weather-code/drizzle'
+import { FogDayIcon } from './weather-code/fog-day'
+import { FogNightIcon } from './weather-code/fog-night'
+import { FreezingRainIcon } from './weather-code/freezing-rain'
+import { PartlyCloudDayIcon } from './weather-code/partly-cloud-day'
+import { PartlyCloudNightIcon } from './weather-code/partly-cloud-night'
+import { RainIcon } from './weather-code/rain'
+import { SnowIcon } from './weather-code/snow'
+import { SnowShowersIcon } from './weather-code/snow-showers'
+import { ThunderstormIcon } from './weather-code/thunderstorm'
+//weather-stats
+import { HumidityIcon } from './weather-stats/humidity'
+import { PressureIcon } from './weather-stats/pressure'
+import { WindIcon } from './weather-stats/wind'
+import { WindDirectIcon } from './weather-stats/wind-direct'
 
 export {
   ClearDayIcon,
@@ -27,3 +33,5 @@ export {
   SnowShowersIcon,
   ThunderstormIcon
 }
+
+export { HumidityIcon, PressureIcon, WindDirectIcon, WindIcon }
