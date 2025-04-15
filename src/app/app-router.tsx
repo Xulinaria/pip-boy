c3db51dc592da978c63ff88c247e9afa72a7dc8b
Xulik <visahomet@gmail.com>
2025-04-15 10:07:30 +0300

    feat(app-router): update app-router for app-layout and add links on pages

--- src/app/app-router.tsx
+++ src/app/app-router.tsx
@@ -3,18 +3,56 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import { Error } from '@pages/error'
 import { Home } from '@pages/home'
 
+import { AppLayout } from './app-layout'
+
+const IN_DEV = <>В разработке</>
+
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          index
-          element={<Home />}
-        />
-        <Route
-          path='/*'
-          element={<Error />}
-        />
+        <Route element={<AppLayout />}>
+          <Route
+            index
+            element={<Home />}
+          />
+          <Route
+            path='/habit'
+            element={IN_DEV}
+          />
+          <Route
+            path='/economy'
+            element={IN_DEV}
+          />
+          <Route
+            path='/trello'
+            element={IN_DEV}
+          />
+          <Route
+            path='/book'
+            element={IN_DEV}
+          />
+          <Route
+            path='/time-tracker'
+            element={IN_DEV}
+          />
+          <Route
+            path='/game'
+            element={IN_DEV}
+          />
+          <Route
+            path='/about'
+            element={IN_DEV}
+          />
+          <Route
+            path='/version'
+            element={IN_DEV}
+          />
+          <Route
+            path='/*'
+            element={<Error />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
